cb862b78c4d4b38264b479578590540649fb321a
aluno <aluno@udicentro.local>
2022-09-19 21:53:38 -0300

    Primeiro

---
+++ Eureka/src/test/java/br/edu/iftm/Eureka/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
